f62a6d19df677e882de4abc7e9582c4d5ac79291
TakuyaTaniguchi <makubex.tt@gmail.com>
2025-10-24 13:51:23 +0900

    MCPServerを起動する

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { StreamableHTTPTransport } from '@hono/mcp'
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
+import {ucs2} from "node:punycode";
 
 const app = new Hono()
+
+const mcpServer = new McpServer({
+  name: 'hono-mcp-server-file-api',
+    version: '1.0.0',
+})
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-app.get('/mcp', (c) => {
-  return c.text('Hello MCP!')
+app.all('/mcp', async (c) => {
+  const transport = new StreamableHTTPTransport()
+  await mcpServer.connect( transport)
+  return transport.handleRequest(c)
 })
 
 serve({
